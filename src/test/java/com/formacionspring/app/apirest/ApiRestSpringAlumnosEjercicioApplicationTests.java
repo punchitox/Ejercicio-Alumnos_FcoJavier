fe3bbd7ebcb5de702cb080282d02e799e5bbf74c
punchitox <fjgarciabasanta@gmail.com>
2022-02-14 15:39:15 +0100

    primer commit. creacion proyecto

---
+++ src/test/java/com/formacionspring/app/apirest/ApiRestSpringAlumnosEjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionspring.app.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringAlumnosEjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
