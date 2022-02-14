61f4e083d75f49c907c34baaee3bfadf428a2976
punchitox <fjgarciabasanta@gmail.com>
2022-02-14 15:58:40 +0100

    Entidad Alumno

--- src/main/java/com/formacionspring/app/apirest/dao/AlumnoDao.java
+++ src/main/java/com/formacionspring/app/apirest/dao/AlumnoDao.java
@@ -1,5 +1,17 @@
 package com.formacionspring.app.apirest.dao;
 
-public class AlumnoDao {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.formacionspring.app.apirest.entity.Alumno;
+import com.formacionspring.app.apirest.entity.Comunidad;
+
+
+public interface AlumnoDao extends CrudRepository<Alumno, Long> {
+
+	@Query("from comunidad")
+	public List<Comunidad> findAllComunidades();
+	
 }
--- src/main/java/com/formacionspring/app/apirest/entity/Alumno.java
+++ src/main/java/com/formacionspring/app/apirest/entity/Alumno.java
@@ -1,5 +1,120 @@
 package com.formacionspring.app.apirest.entity;
 
-public class Alumno {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table( name="alumnos" )
+public class Alumno implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String nombre, apellido, email, direccion, imagen;
+	@Column(nullable = false, unique = true)
+	private String dni;
+	private int cp, telefono;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="comunidad_id")
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	private Comunidad comunidad;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public int getCp() {
+		return cp;
+	}
+
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public Comunidad getComunidad() {
+		return comunidad;
+	}
+
+	public void setComunidad(Comunidad comunidad) {
+		this.comunidad = comunidad;
+	}
+	
 }
