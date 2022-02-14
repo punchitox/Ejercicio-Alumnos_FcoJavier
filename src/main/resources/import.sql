39708c9a8ded2a89a6c7a16c5bc1ca4d236a0d06
punchitox <fjgarciabasanta@gmail.com>
2022-02-14 16:35:04 +0100

    import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO comunidades(nombre) VALUES ('Comunidad de Madrid');
+INSERT INTO comunidades(nombre) VALUES ('Comunidad Valenciana');
+INSERT INTO comunidades(nombre) VALUES ('Principado de Asturias');
+INSERT INTO comunidades(nombre) VALUES ('Galicia');
+INSERT INTO comunidades(nombre) VALUES ('Aragon');
+INSERT INTO comunidades(nombre) VALUES ('Andalucia');
+INSERT INTO comunidades(nombre) VALUES ('Extremadura');
+INSERT INTO comunidades(nombre) VALUES ('Pais Vasco');
+
+INSERT INTO alumnos(comunidad_id,nombre,apellido,email,dni,telefono) VALUES(1,"Francisco","Garcia","fg@gmail.com","54762147U", 65478524);
+INSERT INTO alumnos(comunidad_id,nombre,apellido,email,dni,telefono) VALUES(1,"Rojas","Cabello","rb@gmail.com","5674135R", 4789625);
+INSERT INTO alumnos(comunidad_id,nombre,apellido,email,dni,telefono) VALUES(1,"Francisco","Garcia","fg@gmail.com","7746565M", 423698);
+INSERT INTO alumnos(comunidad_id,nombre,apellido,email,dni,telefono) VALUES(1,"Francisco","Garcia","fg@gmail.com","47896L", 547899);
