2c7e548c221ef3b229b06dfb3abe943f627269a6
punchitox <fjgarciabasanta@gmail.com>
2022-02-14 16:00:29 +0100

    Entidad Comunidad

--- src/main/java/com/formacionspring/app/apirest/entity/Comunidad.java
+++ src/main/java/com/formacionspring/app/apirest/entity/Comunidad.java
@@ -1,5 +1,34 @@
 package com.formacionspring.app.apirest.entity;
 
-public class Comunidad {
+import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Comunidad implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
